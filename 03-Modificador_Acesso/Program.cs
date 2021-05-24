9f118da53689f8331810e268538456e2f95c6bd0
Rodrigo Santos Costa <AB1214210@br.atento.com>
2021-05-24 16:20:06 -0300

    Modificadores de acesso

---
+++ 03-Modificador_Acesso/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03_Modificador_Acesso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciando a classe produto
+            Produto produto = new Produto();
+
+            // Só é exibido o atributo nome, já que o atributo valor é privado a classe Produto
+            Console.WriteLine("Digite um nome: ");
+            produto.nome = Console.ReadLine();
+            Console.WriteLine(produto.nome);        
+        }
+    }
+}
