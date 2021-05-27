179bed4ef9b28524f82e5fb457363d3368886b9d
Rodrigo Santos Costa <66096527+rodrigoscosta@users.noreply.github.com>
2021-05-27 13:04:34 -0300

    Conceitos de encapsulamento

--- 04-Encapsulamento/Program.cs
+++ 04-Encapsulamento/Program.cs
@@ -8,19 +8,7 @@ namespace _04_Encapsulamento
         {
             // Instancia da classe Aluno
             Aluno obj = new Aluno();
-
-            /*Quando é gerado objeto da classe Aluno,
-             * os métodos e atributos em private criados nesta classe
-             * não são visíveis em outras classes.
-             * No exemplo abaixo, só foi possivel chamar
-             * o método mensagem, pois está marcado como public.
-             * O programa funcionará normalmente.
-             *
-             *
-             *
-             *
-             *
-             */
+            
             obj.mensagem();
         }
     }
