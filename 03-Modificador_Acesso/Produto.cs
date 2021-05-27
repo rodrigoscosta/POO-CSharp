cf9cf7a26c29a03f7b70e2a8d3e456ffc97a78e8
Rodrigo Santos Costa <66096527+rodrigoscosta@users.noreply.github.com>
2021-05-27 13:09:45 -0300

    Modificadores de acesso

--- 03-Modificador_Acesso/Produto.cs
+++ 03-Modificador_Acesso/Produto.cs
@@ -1,9 +1,9 @@
 public class Produto
 {
     /* Os modificadores de acesso são
-     *      public - Atributos e métodos visiveis em qualquer classe.
-     *      private - Atributos e métodos visiveis apenas na classe em que foram criados.
-     *      protected - Atributos e métodos visiveis em classes onde são criados ou herdados.
+     * public - Atributos e métodos visiveis em qualquer classe.
+     * private - Atributos e métodos visiveis apenas na classe em que foram criados.
+     * protected - Atributos e métodos visiveis em classes onde são criados ou herdados.
      */
 
      public string nome;
